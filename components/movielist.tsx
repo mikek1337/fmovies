2a2a204d0972dbbe2608e6bd70f2dad77b2765e7
mikek1337 <mikekegnlyew@gmail.com>
2024-09-15 20:52:50 +0300

    added links to movie and series cards

--- components/movielist.tsx
+++ components/movielist.tsx
@@ -1,6 +1,7 @@
 import { MovieResponse } from "@/app/types/moviedbresponse"
 import { Play } from "lucide-react"
 import Image from "next/image"
+import Link from "next/link"
 import { FC } from "react"
 interface MovieListProps{
     movies: MovieResponse,
@@ -10,7 +11,8 @@ const MovieList:FC<MovieListProps> = ({movies, mediaType}) => {
     return(
         <div className="grid grid-cols-8 gap-5">
             {movies.results?.map((movie)=>(
-                <div className="border h-fit" key={movie.id}>
+                <Link href={`/home/watch/${movie.media_type==='tv'?'series':'movie'}/${movie.id}`} key={movie.id}>
+                <div className="border h-fit">
                     <div className="relative rounded-md group w-full">
                         <Image src={`http://image.tmdb.org/t/p/w500${movie.poster_path}`} className='object-contain  rounded-md w-full' width={500} height={500} alt={movie.title}/>
                         <span className="absolute top-1 right-1 text-xs font-semibold bg-indigo-900 text-white rounded-lg px-1">{mediaType=='tv'?"TV":"Movie"}</span>
@@ -20,6 +22,7 @@ const MovieList:FC<MovieListProps> = ({movies, mediaType}) => {
                         <span className="font-semibold">{mediaType=="tv"?movie.name:movie.title}</span>
                     </div>
                 </div>
+                </Link>
 
             ))}
         </div>
