6288362d89dab841401a9d29e8bdbe9ee4052736
mikek1337 <mikekegnlyew@gmail.com>
2024-10-05 21:35:37 +0300

    fix: Handle optional values in user data retrieval and avatar rendering

--- components/userprofile.tsx
+++ components/userprofile.tsx
@@ -1,7 +1,14 @@
 "use client";
 
 import { Camera, Loader, UserIcon } from "lucide-react";
-import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger } from "./ui/sheet";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
@@ -16,77 +23,105 @@ import { useSession } from "next-auth/react";
 import { useToast } from "@/hooks/use-toast";
 
 const UserProfile = () => {
-    const session = useSession();
-    const {toast} = useToast();
-    const {isPending, data} = useQuery({
-        queryKey: ["profile"],
-        queryFn: async () =>{
-            const res = await axios.get('/api/user?id='+session.data?.user.id);
-            return res.data as User;
-        },
-        throwOnError(error, query) {
-            
-            toast({
-                title:"error",
-                description:error.message,
-                variant:"destructive"
-            })
-            query.fetch()
-        },
-    });
-    const {register, formState:{errors}} = useForm<ProfileType>({resolver:zodResolver(Profile)});
-    if(!session.data)
-        return null;
-    return(
-    
-        <Sheet defaultOpen>
-            <SheetTrigger >
-                <UserIcon className="w-6 h-6 cursor-pointer rounded-full p-1 hover:shadow-md"/>
-                Porfile
-            </SheetTrigger>
-            <SheetContent side="left">
-                <SheetHeader>
-                    <SheetTitle>User Profile</SheetTitle>
-                </SheetHeader>
-                <SheetDescription>
-                    {isPending && <Loader className="w-5 h-5 animate-spin mx-auto"/>}
-                </SheetDescription>
-                <div>
-                    <div className="relative group mb-10">
-                        <Avatar className="border mx-auto w-[100px] h-[100px]">
-                            <AvatarImage src={data?.image} alt={data?.username} width={100} height={100}/>
-                            <AvatarFallback>{data?.username[0]}</AvatarFallback>
-                        </Avatar>
-                        <div className="absolute bottom-1 bg-white/35 ">
-                            <Camera className="w-5 h-5 text-white"/>
-                        </div>
-                    </div>
-                    <form>
-                        <div className="my-2">
-                            <Input type="text" className="hidden" {...register("image")}/>
-                        </div>
-                        <div className="my-2">
-                            <Input type="text" placeholder="Username" {...register("username")}/>
-                            {errors.username && <FadeText className="text-red-500 font-semibold text-xs" text={errors.username.message}/>}
-                        </div>
-                        <div className="my-2">
-                            <div className="my-2">
-                                <Input type="password" placeholder="password" {...register("password")}/>
-                                {errors.password && <FadeText className="text-red-500 font-semibold text-xs" text={errors.password.message}/>}
-                            </div>
-                            <div className="my-2">
-                                <Input type="password" placeholder="Confirm Password" {...register("confirmPassword")}/>
-                                {errors.confirmPassword && <FadeText className="text-red-500 font-semibold text-xs" text={errors.confirmPassword.message}/>}
-                            </div>
-                        </div>
-                        <div className="my-2">
-                            <Button type="submit" variant="outline" className="w-full">Update Profile</Button>
-                        </div>
-                    </form>
-                </div>
-            </SheetContent>
-        </Sheet>
-    )
-}
+  const session = useSession();
+  const { toast } = useToast();
+  const { isPending, data } = useQuery({
+    queryKey: ["profile"],
+    queryFn: async () => {
+      if (session.data?.user) {
+        const res = await axios.get("/api/user?id=" + "1");
+        return res.data as User;
+      }
+    },
+  });
+  const {
+    register,
+    formState: { errors },
+  } = useForm<ProfileType>({ resolver: zodResolver(Profile) });
+  if (!session.data) return null;
+  return (
+    <Sheet defaultOpen>
+      <SheetTrigger>
+        <UserIcon className="w-6 h-6 cursor-pointer rounded-full p-1 hover:shadow-md" />
+        Porfile
+      </SheetTrigger>
+      <SheetContent side="left">
+        <SheetHeader>
+          <SheetTitle>User Profile</SheetTitle>
+        </SheetHeader>
+        <SheetDescription>
+          {isPending && <Loader className="w-5 h-5 animate-spin mx-auto" />}
+        </SheetDescription>
+        <div>
+          <div className="relative group mb-10">
+            <Avatar className="border mx-auto w-[100px] h-[100px]">
+              <AvatarImage
+                src={data?.image || ""}
+                alt={data?.username || ""}
+                width={100}
+                height={100}
+              />
+              <AvatarFallback>{data?.username || ""}</AvatarFallback>
+            </Avatar>
+            <div className="absolute bottom-1 bg-white/35 ">
+              <Camera className="w-5 h-5 text-white" />
+            </div>
+          </div>
+          <form>
+            <div className="my-2">
+              <Input type="text" className="hidden" {...register("image")} />
+            </div>
+            <div className="my-2">
+              <Input
+                type="text"
+                placeholder="Username"
+                {...register("username")}
+              />
+              {errors.username && (
+                <FadeText
+                  className="text-red-500 font-semibold text-xs"
+                  text={errors.username.message || ""}
+                />
+              )}
+            </div>
+            <div className="my-2">
+              <div className="my-2">
+                <Input
+                  type="password"
+                  placeholder="password"
+                  {...register("password")}
+                />
+                {errors.password && (
+                  <FadeText
+                    className="text-red-500 font-semibold text-xs"
+                    text={errors.password.message || ""}
+                  />
+                )}
+              </div>
+              <div className="my-2">
+                <Input
+                  type="password"
+                  placeholder="Confirm Password"
+                  {...register("confirmPassword")}
+                />
+                {errors.confirmPassword && (
+                  <FadeText
+                    className="text-red-500 font-semibold text-xs"
+                    text={errors.confirmPassword.message || ""}
+                  />
+                )}
+              </div>
+            </div>
+            <div className="my-2">
+              <Button type="submit" variant="outline" className="w-full">
+                Update Profile
+              </Button>
+            </div>
+          </form>
+        </div>
+      </SheetContent>
+    </Sheet>
+  );
+};
 
 export default UserProfile;
