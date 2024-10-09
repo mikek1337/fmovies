29ebeb0f20167d027d9f0744f4e08afd71bc2345
Mikek1337 <mikekegnlyew@gmail.com>
2024-10-09 15:53:33 +0300

    feat: Add pagination controls to Series page and improve loading state handling

--- app/home/series/page.tsx
+++ app/home/series/page.tsx
@@ -7,6 +7,7 @@ import MovieList from "@/components/movielist";
 import SelectGener from "@/components/selectgener";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { cn } from "@/lib/utils";
 import axios from "axios";
 import { Loader } from "lucide-react";
 import { useEffect, useState } from "react";
@@ -53,18 +54,22 @@ const Page = () =>{
                 <h1 className="font-extrabold">Series</h1>
               </div>
               <hr className="my-5"/>
-              <div className="w-full">
+              <div className={cn("w-full px-3", {"min-h-screen":loading})}>
                 {
                     loading && <Loader className="w-5 h-5 animate-spin mx-auto"/>
                 }
                 {
-                    !loading && movies && <MovieList movies={movies} mediaType="tv"/>
+                    !loading && movies && (
+                    <>
+                        <MovieList movies={movies} mediaType="tv"/>
+                    <div className="flex items-center justify-center gap-2 my-2">
+                        <Button onClick={()=>page < 1 ? setPage(page-1):-1} className="bg-indigo-700 text-white">Previous</Button>
+                        <span>{page}/{movies?.total_pages}</span>
+                        <Button onClick={()=>movies?.total_pages && page < movies?.total_pages?setPage(page+1):-1} className="bg-indigo-700 text-white">Next</Button>
+                    </div>
+                    </>
+                )
                 }
-                <div className="flex items-center justify-center gap-2 my-2">
-                    <Button onClick={()=>page < 1 ? setPage(page-1):-1} className="bg-indigo-700 text-white">Previous</Button>
-                    <span>{page}/{movies?.total_pages}</span>
-                    <Button onClick={()=>movies?.total_pages && page < movies?.total_pages?setPage(page+1):-1} className="bg-indigo-700 text-white">Next</Button>
-                </div>
               </div>
        </div>
     )
