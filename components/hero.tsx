cbfbb5b517c3487f0af6f909aa8bfb325c1a1e33
Mikek1337 <mikekegnlyew@gmail.com>
2024-10-10 09:26:46 +0300

    feat: Enhance Hero component styling for improved responsiveness and readability

--- components/hero.tsx
+++ components/hero.tsx
@@ -41,26 +41,26 @@ const Hero:FC<HeroProps> = ({movies})=>{
                 <BlurFade inView key={movie?.id}>
                     <div  className={cn('relative  w-full justify-center md:flex ')}>
                         <div >
-                            <div className='absolute md:relative w-full h-full  bg-gradient-to-r from-indigo-500'>
+                            <div className='absolute md:relative w-full h-full md:bg-none  bg-gradient-to-r from-indigo-500'>
 
                                 <div className=' px-5 flex items-center w-full h-full'>
                                     <div className='flex flex-col gap-2 px-10'>
-                                        <span className='text-5xl font-extrabold  text-white'>{movie?.media_type=="movie"?movie?.title:movie?.name}</span>
-                                        <p className='text-sm max-w-[500px]  line-clamp-4 text-white'>{movie?.overview}</p>
+                                        <span className='md:text-5xl text-2xl font-extrabold md:text-black  text-white'>{movie?.media_type=="movie"?movie?.title:movie?.name}</span>
+                                        <p className='text-sm max-w-[500px]  md:line-clamp-4 line-clamp-2  md:text-black  text-white'>{movie?.overview}</p>
                                         <div className='flex items-center gap-5 md:flex-nowrap flex-wrap'>
                                             <Link className={cn(buttonVariants({variant:'default'}),'flex items-center gap-2 w-fit')} href={movie?.media_type=='movie'?`home/watch/movie/${movie?.id}`:`home/watch/series/${movie?.id}`}>
                                             <Play className='w-5 h-5 fill-white'/>
                                             Watch now
                                             </Link>
                                             <span className='text-xs font-semibold rounded-lg p-1 bg-indigo-900 text-white'>{new Date(movie?.media_type=="tv" ? movie?.first_air_date: movie?.release_date).toDateString()}</span>
-                                            <span className='flex items-center gap-1 text-sm font-semibold'><Stars className='text-yellow-400 fill-yellow-400 w-5 h-5'/>{movie.vote_average?movie.vote_average.toFixed(1):0.0}</span>
+                                            <span className='flex items-center gap-1 text-sm font-semibold md:text-black text-white'><Stars className='text-yellow-400 fill-yellow-400 w-5 h-5'/>{movie.vote_average?movie.vote_average.toFixed(1):0.0}</span>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <div className='border'>
-                            <Image src={`http://image.tmdb.org/t/p/original${movie?.backdrop_path}`} className='object-cover md:h-[650px] w-full  rounded-md' width={1200} height={500} alt={movie?.title}/>
+                        <div className='border md:h-[400px] md:max-w-[700px]'>
+                            <Image src={`http://image.tmdb.org/t/p/original${movie?.backdrop_path}`} className='object-cover md:max-h-[650px] w-full  rounded-md' width={1200} height={500} alt={movie?.title}/>
                         </div>
                     </div>
                     </BlurFade>
