0f56a1a73a1664c424def43904edefdc11bda898
Mikek1337 <mikekegnlyew@gmail.com>
2024-10-11 19:09:15 +0300

    feat: Implement MultiSelect component and integrate with SelectGener for enhanced multi-selection functionality

--- components/selectgener.tsx
+++ components/selectgener.tsx
@@ -1,55 +1,86 @@
-"use client"
+"use client";
 
-import { useQuery } from "@tanstack/react-query"
-import axios from "axios"
-import { ChevronDown, ChevronUp } from "lucide-react"
-import { Gener } from "@/app/types/gener"
-import { useState } from "react"
-import { cn } from "@/lib/utils"
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { ChevronDown, ChevronUp } from "lucide-react";
+import { Gener } from "@/app/types/gener";
+import { useState } from "react";
+import { cn } from "@/lib/utils";
+import React from "react";
+import {
+  MultiSelect,
+  MultiSelectContent,
+  MultiSelectOption,
+  MultiSelectTrigger,
+  MultiSelectValue,
+} from "./multiselect";
 
-const SelectGener = () =>{
-    const[open, setOpen] = useState(false);
-    const[generNames, setGenerNames] = useState<string[]>([]);
-    const {data} = useQuery({
-        queryKey:["geners"],
-        queryFn:async()=>{
-            return await (await axios("/api/geners")).data;
-        },
-        retry:(failureCount, error)=>{
-            if(error.cause === 404) return false;
-            return failureCount < 3;
-        }
-    });
-    const onCheck = (e:React.ChangeEvent<HTMLInputElement>)=>{
-        if(e.target.checked){
-       
-            setGenerNames(prev=>[...prev, e.target.id]);
-        } else {
-           
-            setGenerNames(prev=>prev.filter((name)=>name !== e.target.id));
-        }
+const SelectGener = () => {
+  const [open, setOpen] = useState(false);
+  const [generNames, setGenerNames] = useState<string[]>([]);
+  const { data } = useQuery({
+    queryKey: ["geners"],
+    queryFn: async () => {
+      return await (
+        await axios("/api/geners")
+      ).data;
+    },
+    retry: (failureCount, error) => {
+      if (error.cause === 404) return false;
+      return failureCount < 3;
+    },
+  });
+  const onCheck = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.checked) {
+      setGenerNames((prev) => [...prev, e.target.id]);
+    } else {
+      setGenerNames((prev) => prev.filter((name) => name !== e.target.id));
     }
-    return(
-       <div className="relative">
-        <div className="flex cursor-pointer w-fit justify-between items-center border rounded-lg p-2" onClick={()=>setOpen(!open)}>
-          <span className="line-clamp-1 max-w-[100px]">{generNames.length==0?"Select Gener":generNames.join(",")}</span>
-          {open?<ChevronUp className="w-5 h-5"/>:<ChevronDown className="w-5 h-5"/>}
+  };
+  return (
+    <div className="relative">
+      <div
+        className="flex cursor-pointer w-fit justify-between items-center border rounded-lg p-2"
+        onClick={() => setOpen(!open)}
+      >
+        <span className="line-clamp-1 max-w-[100px]">
+          {generNames.length == 0 ? "Select Gener" : generNames.join(",")}
+        </span>
+        {open ? (
+          <ChevronUp className="w-5 h-5" />
+        ) : (
+          <ChevronDown className="w-5 h-5" />
+        )}
+      </div>
+      <div
+        className={cn(
+          "absolute z-10 flex items-center gap-5 bg-white  rounded-md shadow-md",
+          { hidden: !open }
+        )}
+      >
+        <div className="grid grid-cols-3 items-center gap-1 flex-wrap  w-[500px] p-2">
+          {data?.map((gener: Gener) => (
+            <div className="flex items-center gap-2" key={gener.id}>
+              <input type="checkbox" className="rounded-md" />
+              <label className="text-sm">{gener.name}</label>
+            </div>
+          ))}
         </div>
-        <div className={cn("absolute z-10 flex items-center gap-5 bg-white  rounded-md shadow-md", {"hidden":!open})}>
-          <div className="grid grid-cols-3 items-center gap-1 flex-wrap  w-[500px] p-2">
-            {
-                data?.map((gener:Gener)=>(
-                    <div key={gener.id} className="flex items-center gap-2">
-                        <input type="checkbox" className="rounded-md" value={gener.id} id={`${gener.name}`} onChange={onCheck}/>
-                        <label htmlFor={`${gener.name}`} className="text-sm">{gener.name}</label>
-                    </div>
-                ))
-            }
-          </div>
-          </div>
-
-       </div> 
-    )
-}
+      </div>
+      <MultiSelect>
+        <MultiSelectTrigger>
+          <MultiSelectValue placeholder="Select Gener" />
+        </MultiSelectTrigger>
+        <MultiSelectContent>
+          {data?.map((gener: Gener) => (
+            <MultiSelectOption value={gener.id.toString()} key={gener.id}>
+              {gener.name}
+            </MultiSelectOption>
+          ))}
+        </MultiSelectContent>
+      </MultiSelect>
+    </div>
+  );
+};
 
 export default SelectGener;
